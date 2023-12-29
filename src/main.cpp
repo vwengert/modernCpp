f42ff6bc9aec68ce12c21435b28db7ce2fc4c33d
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2023-12-29 12:08:40 +0100

    Revert "reformat and remove not needed include"
    This reverts commit 793ce56cc4748be002b585f3ff92ab536bb7b59a.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <memory>
 #include <openGL.h>
 
 auto main() -> int
 {
   OpenGL engine( 800, 600, "modern C++" );
-  engine.createShaderProgram( { OpenGL::createAndCompileShader( "#version 330 core\n"
-                                                                "layout (location = 0) in vec3 aPos;\n"
-                                                                "void main()\n"
-                                                                "{\n"
-                                                                " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-                                                                "}\n",
-                                  GL_VERTEX_SHADER ),
+  engine.createShaderProgram( {
     OpenGL::createAndCompileShader( "#version 330 core\n"
-                                    "out vec4 FragColor;\n"
-                                    "void main()\n"
-                                    "{\n"
-                                    " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                    "}\n",
-      GL_FRAGMENT_SHADER ) } );
+                                   "layout (location = 0) in vec3 aPos;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+                                   "}\n", GL_VERTEX_SHADER ),
+    OpenGL::createAndCompileShader( "#version 330 core\n"
+                                     "out vec4 FragColor;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     " FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                     "}\n", GL_FRAGMENT_SHADER )
+  } );
   float vertices[] = { -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f };
 
   engine.prepareVertices( vertices, sizeof vertices );
