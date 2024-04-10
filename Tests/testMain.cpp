d1fb6eed2a9a6828b9ad85b2b24ba356b1ef8360
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-04-10 07:28:03 +0200

    adding test for counting objects

--- Tests/testMain.cpp
+++ Tests/testMain.cpp
@@ -2,11 +2,40 @@
 
 #include "singleton.h"
 
-TEST(ModernTests, singletonReturnsConfiguration) {
-  ASSERT_TRUE(Singleton::instance()->configuration() == "Configuration");
+TEST( ModernTests, singletonReturnsConfiguration )
+{
+  ASSERT_TRUE( Singleton::instance()->configuration() == "Configuration" );
 }
 
-TEST(ModernTests, palindromIsFalseWhenNoPalindrom) {
-  ASSERT_FALSE(false);
-  ASSERT_FALSE(false);
+TEST( ModernTests, palindromIsFalseWhenNoPalindrom )
+{
+  ASSERT_FALSE( false );
+  ASSERT_FALSE( false );
+}
+
+struct object_counter
+{
+    static inline int count;
+    static inline int all_count;
+    object_counter()
+    {
+      ++count;
+      ++all_count;
+    }
+    ~object_counter()
+    {
+      --count;
+    }
+};
+
+TEST( ScopedPtrTest, Construct )
+{
+  object_counter::all_count = 0;
+  object_counter::count = 0;
+  auto p = new object_counter;
+  EXPECT_EQ( 1, object_counter::count );
+  EXPECT_EQ( 1, object_counter::all_count );
+  delete p;
+  EXPECT_EQ( 0, object_counter::count );
+  EXPECT_EQ( 1, object_counter::all_count );
 }
