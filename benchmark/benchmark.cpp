3c5ca3211188eef9f309b1dd45926c60f485d6c7
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-04-11 07:18:52 +0200

    adding benchmark for pointer stuff

---
+++ benchmark/benchmark.cpp
@@ -0,0 +1,40 @@
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <memory>
+#include <raiitemplates.h>
+
+void BM_rawptr_dereference( benchmark::State& state )
+{
+  int* p = new int;
+  for( auto _ : state )
+  {
+    benchmark::DoNotOptimize( *p );
+  }
+  delete p;
+  state.SetItemsProcessed( 32 * state.iterations() );
+}
+
+void BM_scoped_ptr_dereference( benchmark::State& state )
+{
+  scoped_ptr< int > p( new int );
+  for( auto _ : state )
+  {
+    benchmark::DoNotOptimize( *p );
+  }
+  state.SetItemsProcessed( 32 * state.iterations() );
+}
+
+void BM_unique_ptr_dereference( benchmark::State& state )
+{
+  std::unique_ptr< int > p( new int );
+  for( auto _ : state )
+  {
+    benchmark::DoNotOptimize( *p );
+  }
+  state.SetItemsProcessed( 32 * state.iterations() );
+}
+
+BENCHMARK( BM_rawptr_dereference );
+BENCHMARK( BM_scoped_ptr_dereference );
+BENCHMARK( BM_unique_ptr_dereference );
+BENCHMARK_MAIN();
