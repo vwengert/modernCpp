eb05ae967ab46fc6eb3a1ac4fd5e7b246a5821cc
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-09-01 10:36:28 +0200

    max template with specified return type

--- mainlib/include/learningtemplate.h
+++ mainlib/include/learningtemplate.h
@@ -1,7 +1,13 @@
 #pragma once
 
-template< typename T >
-constexpr T maxTemplate( T a, T b )
+template< typename T1, typename T2 >
+constexpr auto maxTemplate( T1 a, T2 b )
+{
+  return b < a ? a : b;
+}
+
+template< typename T1, typename T2, typename RT = std::decay_t< decltype( true ? T1() : T2() ) > >
+constexpr RT maxSpecifiedRTTemplate( T1 a, T2 b )
 {
   return b < a ? a : b;
 }
