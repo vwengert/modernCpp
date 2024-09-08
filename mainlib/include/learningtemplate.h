31272aebd2181d448ddf6aa1d5d60f5f3c029945
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-09-08 11:42:13 +0200

    make container as template

--- mainlib/include/learningtemplate.h
+++ mainlib/include/learningtemplate.h
@@ -12,10 +12,10 @@ constexpr RT maxTemplate( T1 first, T2 second )
   return second < first ? first : second;
 }
 
-template< typename T >
+template< typename T, typename Cont >
 class Stack;
-template< typename T >
-std::ostream& operator<<( std::ostream& stream, Stack< T > const& stack )
+template< typename T, typename Cont >
+std::ostream& operator<<( std::ostream& stream, Stack< T, Cont > const& stack )
 {
   for( auto item : stack.m_elems )
   {
@@ -25,7 +25,7 @@ std::ostream& operator<<( std::ostream& stream, Stack< T > const& stack )
   return stream;
 }
 
-template< typename T >
+template< typename T, typename Cont = std::vector< T > >
 class Stack
 {
   public:
@@ -40,24 +40,24 @@ class Stack
     friend std::ostream& operator<< < T >( std::ostream&, Stack< T > const& );
 
   private:
-    std::vector< T > m_elems;
+    Cont m_elems;
 };
 
-template< typename T >
-void Stack< T >::push( T const& elem )
+template< typename T, typename Cont >
+void Stack< T, Cont >::push( T const& elem )
 {
   m_elems.push_back( elem );
 }
 
-template< typename T >
-void Stack< T >::pop()
+template< typename T, typename Cont >
+void Stack< T, Cont >::pop()
 {
   assert( !m_elems.empty() );
   m_elems.pop_back();
 }
 
-template< typename T >
-T const& Stack< T >::top() const
+template< typename T, typename Cont >
+T const& Stack< T, Cont >::top() const
 {
   assert( !m_elems.empty() );
   return m_elems.back();
