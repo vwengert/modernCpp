5d347d2c3e5ddfaa210ccced6983b4188a5f4111
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-09-08 08:23:41 +0200

    fixing clang-tidy warnings

--- mainlib/include/learningtemplate.h
+++ mainlib/include/learningtemplate.h
@@ -1,22 +1,22 @@
 #pragma once
 
 template< typename T >
-constexpr auto maxTemplate( T a, T b )
+constexpr auto maxTemplate( T first, T second )
 {
-  return b < a ? a : b;
+  return second < first ? first : second;
 }
 
 template< typename T1, typename T2, typename RT = std::decay_t< decltype( true ? T1() : T2() ) > >
-constexpr RT maxTemplate( T1 a, T2 b )
+constexpr RT maxTemplate( T1 first, T2 second )
 {
-  return b < a ? a : b;
+  return second < first ? first : second;
 }
 
 template< typename T >
 class Stack
 {
   private:
-    std::vector< T > elems;
+    std::vector< T > m_elems;
 
   public:
     void push( T const& elem );
@@ -24,26 +24,26 @@ class Stack
     T const& top() const;
     bool empty() const
     {
-      return elems.empty();
+      return m_elems.empty();
     }
 };
 
 template< typename T >
 void Stack< T >::push( T const& elem )
 {
-  elems.push_back( elem );
+  m_elems.push_back( elem );
 }
 
 template< typename T >
 void Stack< T >::pop()
 {
-  assert( !elems.empty() );
-  elems.pop_back();
+  assert( !m_elems.empty() );
+  m_elems.pop_back();
 }
 
 template< typename T >
 T const& Stack< T >::top() const
 {
-  assert( !elems.empty() );
-  return elems.back();
+  assert( !m_elems.empty() );
+  return m_elems.back();
 }
