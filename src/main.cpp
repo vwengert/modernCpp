178065d4aedb05e5649c701456c966290241f2be
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-11-06 13:20:33 +0100

    adding a thread guard to make sure to exit main after thread finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,42 @@
 #include <modernLibrary.h>
 #include <thread>
 
-class BackgroundTask {
-public:
-    void operator()() const {
-        doWork();
+class ThreadGuard
+{
+    std::thread& m_thread;
+
+  public:
+    explicit ThreadGuard( std::thread& thread )
+      : m_thread( thread )
+    {
+    }
+    ~ThreadGuard()
+    {
+      if( m_thread.joinable() )
+      {
+        m_thread.join();
+      }
+    }
+    ThreadGuard( ThreadGuard const& ) = delete;
+    ThreadGuard& operator=( ThreadGuard const& ) = delete;
+    ThreadGuard( ThreadGuard&& ) = delete;
+    ThreadGuard& operator=( ThreadGuard&& ) = delete;
+};
+
+class BackgroundTask
+{
+  public:
+    void operator()() const
+    {
+      doWork();
     }
 };
 
-auto main() -> int {
-    BackgroundTask task;
-    std::thread th1(task);
-    th1.join();
-    return 0;
+auto main() -> int
+{
+  BackgroundTask task;
+  std::thread th1( task );
+  ThreadGuard guard( th1 );
+
+  return 0;
 }
