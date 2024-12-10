61ba994d1ac797a72befebbfc41083f824e438c4
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-10 14:53:23 +0100

    first function template

--- basiclib/include/functiontemplates.h
+++ basiclib/include/functiontemplates.h
@@ -1,1 +1,7 @@
 #pragma once
+
+template< typename T >
+constexpr T max( const T& first, const T& second )
+{
+  return second < first ? first : second;
+}
--- basiclib/src/basicLibrary.cpp
+++ basiclib/src/basicLibrary.cpp
@@ -1,9 +1,17 @@
 #include "basicLibrary.h"
+#include "functiontemplates.h"
 
 #include <iostream>
 
 auto doBasicWork() -> int
 {
+  constexpr auto kFIRST = 3;
+  constexpr auto kSECOND = 7;
+  const std::string firstWord = "Hello";
+  const std::string secondWord = "World";
+
   std::cout << "Basic Library\n";
+  std::cout << "Max from " << kFIRST << " and " << kSECOND << " is " << max( kFIRST, kSECOND ) << '\n';
+  std::cout << "Max from " << firstWord << " and " << secondWord << " is " << ::max( firstWord, secondWord ) << '\n';
   return 0;
 }
