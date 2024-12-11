7d44f457a5ba7c778108167b4823ef9a5a57c809
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-11 14:33:55 +0100

    using functiontemplates in basic library

--- basiclib/src/basicLibrary.cpp
+++ basiclib/src/basicLibrary.cpp
@@ -6,14 +6,24 @@
 auto doBasicWork() -> int
 {
   constexpr auto kFIRST = 5;
-  constexpr auto kSECOND = 7;
+  constexpr auto kSECOND = 3;
   constexpr auto kTHIRD = 5.1;
+  const int* kPtrFirst = &kFIRST;
+  const int* kPtrSecond = &kSECOND;
+  int const& kFirstRef = kFIRST;
   const std::string firstWord = "Hello";
   const std::string secondWord = "World";
+  const auto* kPtrFirstWord = "hello";
+  const auto* kPtrSecondWord = "world";
 
   std::cout << "Basic Library\n";
   std::cout << "Max from " << kFIRST << " and " << kSECOND << " is " << ::max( kFIRST, kSECOND ) << '\n';
   std::cout << "Max from " << firstWord << " and " << secondWord << " is " << ::max( firstWord, secondWord ) << '\n';
   std::cout << "Max from " << kFIRST << " and " << kTHIRD << " is " << ::max( kFIRST, kTHIRD ) << '\n';
+  std::cout << "Max from " << kSECOND << " and " << kFirstRef << " is " << ::max< long double >( kFirstRef, kSECOND )
+            << '\n';
+  std::cout << "Max from " << *kPtrFirst << " and " << *kPtrSecond << " is " << *::max( kPtrFirst, kPtrSecond ) << '\n';
+  std::cout << "Max from " << kPtrFirstWord << " and " << kPtrSecondWord << " is "
+            << ::max( kPtrFirstWord, kPtrSecondWord ) << '\n';
   return 0;
 }
