577945ac2eff8316f91b5751a9071497bda93035
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-12 17:56:50 +0100

    cleanup formatting

--- basiclib/include/functiontemplates.h
+++ basiclib/include/functiontemplates.h
@@ -3,62 +3,52 @@
 #include <type_traits>
 #include <iostream>
 
-inline char const* max( char const* first, char const* second )
-{
-  return std::strcmp( second, first ) < 0 ? first : second;
+inline char const *max(char const *first, char const *second) {
+  return std::strcmp(second, first) < 0 ? first : second;
 }
 
-template< typename T1, typename T2 >
-constexpr auto max( T1 first, T2 second ) -> std::common_type_t< T1, T2 >
-{
+template<typename T1, typename T2>
+constexpr auto max(T1 first, T2 second) -> std::common_type_t<T1, T2> {
   return second < first ? first : second;
 }
 
-template< typename RT, typename T1, typename T2 >
-constexpr RT max( T1 first, T2 second )
-{
+template<typename RT, typename T1, typename T2>
+constexpr RT max(T1 first, T2 second) {
   return second < first ? first : second;
 }
 
-template< typename T >
-T* max( T* first, T* second )
-{
+template<typename T>
+T *max(T *first, T *second) {
   return *second < *first ? first : second;
 }
 
 template<typename T>
-void print(std::ostream& stream, const char* /*sep*/,  const char* end, T arg)
-{
+void print(std::ostream &stream, const char * /*sep*/, const char *end, T arg) {
   stream << arg << end;
 }
 
 template<typename T, typename... Types>
-void print(std::ostream& stream, const char* sep, const char* end, T firstArg, Types... args)
-{
+void print(std::ostream &stream, const char *sep, const char *end, T firstArg, Types... args) {
   stream << firstArg << sep;
   print(stream, sep, end, args...);
 }
 
 template<typename T, typename... Types>
-void print(std::ostream& stream, T firstArg, Types... args)
-{
+void print(std::ostream &stream, T firstArg, Types... args) {
   print(stream, ", ", "\n", firstArg, args...);
 }
 
 template<typename... T>
-constexpr auto foldSum(T... args)
-{
+constexpr auto foldSum(T... args) {
   return (... + args);
 }
 
 template<typename T1, typename... TN>
-constexpr bool isHomogeneous(T1, TN...)
-{
+constexpr bool isHomogeneous(T1, TN...) {
   return (std::is_same_v<T1, TN> && ...);
 }
 
 template<std::size_t... Idx, typename C>
-void printIdx(std::ostream& stream, C const& container)
-{
+void printIdx(std::ostream &stream, C const &container) {
   ((stream << container[Idx] << ' '), ...) << '\n';
 }
