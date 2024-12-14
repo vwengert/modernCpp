0980f56be46a2f39d98f6e4554d4b2fe54aa0d72
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-14 06:32:26 +0100

    fix windows bugs

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,35 @@
 #include "modernLibrary.h"
 #include "moderncpp_version.h"
 #include <iostream>
+#include <mutex>
 #include <thread>
 
-std::mutex mutex;
+std::mutex mutex; // NOLINT(*-avoid-non-const-global-variables)
 
 class ThreadGuard
 {
-    std::thread& m_thread;
+  std::thread& m_thread;
 
   public:
     explicit ThreadGuard( std::thread& thread )
       : m_thread( thread )
     {
     }
+
     ~ThreadGuard()
     {
-      if( m_thread.joinable() )
+      if(m_thread.joinable())
       {
         m_thread.join();
       }
     }
+
     ThreadGuard( ThreadGuard const& ) = delete;
+
     ThreadGuard& operator=( ThreadGuard const& ) = delete;
+
     ThreadGuard( ThreadGuard&& ) = delete;
+
     ThreadGuard& operator=( ThreadGuard&& ) = delete;
 };
 
@@ -33,13 +39,22 @@ class BackgroundTask
   public:
     void operator()() const
     {
+#ifdef WIN32
+      _sleep( 1 );
+#else
       sleep( 1 );
+#endif
       std::lock_guard< std::mutex > guard( mutex );
       doWork();
     }
+
     void operator()( const int value, const std::string& text ) const
     {
+#ifdef WIN32
+      _sleep( 1 );
+#else
       sleep( 1 );
+#endif
       std::lock_guard< std::mutex > guard( mutex );
       doWork();
       std::cout << "Value: " << value << " Text: " << text << std::endl;
