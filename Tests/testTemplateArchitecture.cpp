d7e64bc9cf8b339ab3aea24c0a40b4c309eef7c3
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-19 08:07:07 +0100

    creating static polymorphism example

---
+++ Tests/testTemplateArchitecture.cpp
@@ -0,0 +1,36 @@
+#include <gmock/gmock.h>
+
+#include "geometry.h"
+
+class Cirle
+{
+  public:
+    static void draw( std::ostream& stream )
+    {
+      stream << "Circle" << '\n';
+    }
+};
+
+class Line
+{
+  public:
+    static void draw( std::ostream& stream )
+    {
+      stream << "Line" << '\n';
+    }
+};
+
+
+TEST( staticPolymorphism, myDraw )
+{
+  std::vector< std::variant< Cirle, Line > > vec;
+  vec.emplace_back( Cirle() );
+  vec.emplace_back( Line() );
+  std::stringstream stream;
+  for(const auto& obj : vec)
+  {
+    std::visit( [&stream]( const auto& object ) { myDraw( stream, object ); }, obj );
+  }
+
+  ASSERT_EQ( stream.str(), "Circle\nLine\n" );
+}
