59c59618eac51eb22657c5b3ff24c6d2164a65a3
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-19 15:23:42 +0100

    adding a start value to the traits

--- basiclib/include/traits.h
+++ basiclib/include/traits.h
@@ -6,28 +6,32 @@ struct AccumTraits;
 template<>
 struct AccumTraits<char> {
   using AccT = int;
+  static constexpr AccT kZERO = 0;
 };
 
 template<>
 struct AccumTraits<int> {
   using AccT = long; // NOLINT(*-runtime-int)
+  static constexpr AccT kZERO = 0;
 };
 
 template<>
 struct AccumTraits<unsigned int> {
   using AccT = unsigned long; // NOLINT(*-runtime-int)
+  static constexpr AccT kZERO = 0;
 };
 
 template<>
 struct AccumTraits<float> {
   using AccT = double;
+  static constexpr AccT kZERO = 0.0;
 };
 
 
 template<typename T>
 auto accum(T const *beg, T const *end) {
   using AccT = typename AccumTraits<T>::AccT;
-  AccT total{};
+  AccT total = AccumTraits<T>::kZERO;
   while (beg != end) {
     total += *beg;
     ++beg; // NOLINT(*-pro-bounds-pointer-arithmetic)
