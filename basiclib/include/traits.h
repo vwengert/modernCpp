aea31fd43130e0fefd04840c1a0bc9710e5ccf48
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-19 15:52:10 +0100

    have own Policy and own initial Value for the total

--- basiclib/include/traits.h
+++ basiclib/include/traits.h
@@ -27,13 +27,22 @@ struct AccumTraits<float> {
   static constexpr AccT kZERO = 0.0;
 };
 
+class SumPolicy {
+public:
+  template<typename T1, typename T2>
+  static void accumulate(T1 &total, T2 const &value) {
+    total += value;
+  }
+};
 
-template<typename T>
-auto accum(T const *beg, T const *end) {
+template<typename T,
+  typename Policy = SumPolicy,
+  typename Traits = AccumTraits<T> >
+auto accum(T const *beg, T const *end, T initValue = Traits::kZERO) {
   using AccT = typename AccumTraits<T>::AccT;
-  AccT total = AccumTraits<T>::kZERO;
+  AccT total = initValue;
   while (beg != end) {
-    total += *beg;
+    Policy::accumulate(total, *beg);
     ++beg; // NOLINT(*-pro-bounds-pointer-arithmetic)
   }
   return total;
