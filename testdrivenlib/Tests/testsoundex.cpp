f53f9912d3777507399f0c9ca14a7b733b7f7c9c
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-22 09:18:44 +0100

    Pads with zeros to ensure three digits

--- testdrivenlib/Tests/testsoundex.cpp
+++ testdrivenlib/Tests/testsoundex.cpp
@@ -3,17 +3,34 @@
 class Soundex
 {
   public:
-    std::string encode( const std::string& word )
+    std::string encode( const std::string& word ) const
     {
-      return "A";
+      return zeroPad( word );
+    }
+
+  private:
+    std::string zeroPad( const std::string& word ) const
+    {
+      return word + "000";
     }
 };
 
 using testing::Eq;
 
-TEST( SoundexEncoding, RetainsSoleLetterOfOneLetterWord )
+class SoundexEncoding : public testing::Test
 {
-  Soundex soundx;
-  auto encoded = soundx.encode( "A" );
-  ASSERT_THAT( encoded, Eq("A") );
+  public:
+    Soundex soundx;
+};
+
+TEST_F( SoundexEncoding, RetainsSoleLetterOfOneLetterWord )
+{
+  const auto encoded = soundx.encode( "A" );
+  ASSERT_THAT( encoded, Eq("A000") );
+}
+
+TEST_F( SoundexEncoding, PadsWithZerosToEnsureThreeDigits )
+{
+  const auto encoded = soundx.encode( "I" );
+  ASSERT_THAT( encoded, Eq("I000") );
 }
