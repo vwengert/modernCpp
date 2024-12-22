e183842ed396527d9c132f9963424dce24a0b68c
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-22 18:07:44 +0100

    Replace Multiple Consonants With Digits

--- testdrivenlib/Tests/testsoundex.cpp
+++ testdrivenlib/Tests/testsoundex.cpp
@@ -32,3 +32,8 @@ TEST_F( SoundexEncoding, IgnoresNonAlphabetics )
 {
   ASSERT_THAT( soundex.encode("A#"), Eq("A000") );
 }
+
+TEST_F( SoundexEncoding, ReplacesMultipleConsonantsWithDigits )
+{
+  ASSERT_THAT( soundex.encode("Acdl"), Eq("A234") );
+}
