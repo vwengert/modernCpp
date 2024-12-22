c6c64d64af69764e772c6983db489208984bdcf2
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-22 17:59:27 +0100

    clean up code

--- testdrivenlib/include/soundex.h
+++ testdrivenlib/include/soundex.h
@@ -7,23 +7,23 @@ constexpr size_t kMAX_CODE_LENGTH{4};
 
 class Soundex {
 public:
-  std::string encode(const std::string &word) const {
+  static std::string encode(const std::string &word) {
     return zeroPad(head(word) + encodedDigits(word));
   }
 
 private:
-  std::string head(const std::string &word) const {
+  static std::string head(const std::string &word) {
     return word.substr(0, 1);
   }
 
-  std::string encodedDigits(const std::string &word) const {
+  static std::string encodedDigits(const std::string &word) {
     if (word.length() > 1) {
       return encodedDigit(word[1]);
     }
     return "";
   }
 
-  std::string encodedDigit(char letter) const {
+  static std::string encodedDigit(const char letter) {
     const std::unordered_map<char, std::string> encodings{
       {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
       {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
@@ -32,11 +32,11 @@ private:
       {'m', "5"}, {'n', "5"},
       {'r', "6"}
     };
-    auto it = encodings.find(letter);
+    const auto it = encodings.find(letter);
     return it == encodings.end() ? "" : it->second;
   }
 
-  std::string zeroPad(const std::string &word) const {
+  static std::string zeroPad(const std::string &word) {
     auto zerosNeeded = kMAX_CODE_LENGTH - word.length();
     return word + std::string(zerosNeeded, '0');
   }
