95822dd87df023536bf1f3074afdea9db145e29e
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-22 19:21:56 +0100

    does not apply duplicate digit rule with seperator vowel

--- testdrivenlib/Tests/testsoundex.cpp
+++ testdrivenlib/Tests/testsoundex.cpp
@@ -70,3 +70,8 @@ TEST_F( SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st )
 {
   ASSERT_THAT( soundex.encode("Bbcd"), Eq("B230") );
 }
+
+TEST_F( SoundexEncoding, DoesNotCombineDuplicateEncodingsSeparatedByVowels )
+{
+  ASSERT_THAT( soundex.encode("Jbob"), Eq("J110") );
+}
