36d4e2997e04c6d121d3eb9a032e5d30c0c1535b
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-23 10:51:00 +0100

    is empty again after removing tweet and check with custom matcher

--- testdrivenlib/Tests/testretweetcollection.cpp
+++ testdrivenlib/Tests/testretweetcollection.cpp
@@ -1,18 +1,63 @@
 #include "gmock/gmock.h"
 
+class Tweet
+{
+};
+
 class RetweetCollection
 {
   public:
+    void add( const Tweet& tweet )
+    {
+      mSize = 1;
+    }
+
+    void remove( const Tweet& tweet )
+    {
+      mSize = 0;
+    }
+
     bool isEmpty() const
     {
-      return true;
+      return 0 == size();
     }
+
+    size_t size() const
+    {
+      return mSize;
+    }
+
+  private:
+    size_t mSize{ 0 };
 };
 
 using namespace testing;
 
-TEST( ARetweetCollection, IsEmptyWhenCreated )
+class ARetweetCollection : public Test
 {
-  RetweetCollection retweets;
-  ASSERT_TRUE( retweets.isEmpty() );
+  public:
+    RetweetCollection retweets;
+};
+
+MATCHER_P( HasSize, expected, "" )
+{
+  return arg.size() == expected && arg.isEmpty() == ( 0 == expected );
+}
+
+TEST_F( ARetweetCollection, HasSizeZeroAndIsEmptyWhenCreated )
+{
+  ASSERT_THAT( retweets, HasSize(0U) );
+}
+
+TEST_F( ARetweetCollection, SizeIsOneAfterTweetAddedAndIsNoLongerEmpty )
+{
+  retweets.add( Tweet() );
+  ASSERT_THAT( retweets, HasSize(1U) );
+}
+
+TEST_F( ARetweetCollection, DecreaseSizeAfterRemovingTweet )
+{
+  retweets.add( Tweet() );
+  retweets.remove( Tweet() );
+  ASSERT_THAT( retweets, HasSize(0U) );
 }
