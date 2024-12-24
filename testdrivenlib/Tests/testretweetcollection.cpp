1b862c046f1eba4b3ea74deed85ba0808e4683ba
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-24 06:47:43 +0100

    Refactor tests

--- testdrivenlib/Tests/testretweetcollection.cpp
+++ testdrivenlib/Tests/testretweetcollection.cpp
@@ -37,35 +37,35 @@ class RetweetCollection
 
 using namespace testing;
 
-class ARetweetCollection : public Test
+MATCHER_P( HasSize, expected, "" )
+{
+  return arg.size() == expected && arg.isEmpty() == ( 0 == expected );
+}
+
+TEST( ARetweetCollection, HasSizeZeroAndIsEmptyWhenCreated )
+{
+  const RetweetCollection retweets;
+  ASSERT_THAT( retweets, HasSize(0U) );
+}
+
+class ARetweetCollectionWithOneTweet : public Test
 {
   public:
     RetweetCollection collection;
 
     void SetUp() override
     {
-      const Tweet tweet;
+      Tweet tweet;
       collection.add( tweet );
     }
 };
 
-MATCHER_P( HasSize, expected, "" )
-{
-  return arg.size() == expected && arg.isEmpty() == ( 0 == expected );
-}
-
-TEST_F( ARetweetCollection, HasSizeZeroAndIsEmptyWhenCreated )
-{
-  RetweetCollection retweets;
-  ASSERT_THAT( retweets, HasSize(0U) );
-}
-
-TEST_F( ARetweetCollection, SizeIsOneAfterTweetAddedAndIsNoLongerEmpty )
+TEST_F( ARetweetCollectionWithOneTweet, SizeIsOneAfterTweetAddedAndIsNoLongerEmpty )
 {
   ASSERT_THAT( collection, HasSize(1U) );
 }
 
-TEST_F( ARetweetCollection, DecreaseSizeAfterRemovingTweet )
+TEST_F( ARetweetCollectionWithOneTweet, DecreaseSizeAfterRemovingTweet )
 {
   collection.remove( Tweet() );
   ASSERT_THAT( collection, HasSize(0U) );
