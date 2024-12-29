8a4d8320e5aac2a0f58aba9c62da780c777da948
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-29 16:22:12 +0100

    get price of one item

--- testdrivenlib/Tests/testsupermarketpricing.cpp
+++ testdrivenlib/Tests/testsupermarketpricing.cpp
@@ -1,8 +1,18 @@
 #include "gmock/gmock.h"
 
+class SupermarketPricing
+{
+  public:
+    double getPrice( const char* article )
+    {
+      return 50;
+    }
+};
+
 using namespace testing;
 
-TEST( SupermarketPricing, StartHere )
+TEST( SupermarketPricing, GetPriceOfOneItem )
 {
-  ASSERT_THAT( true, Eq(true) );
+  SupermarketPricing pricing;
+  ASSERT_THAT( pricing.getPrice( "A" ), DoubleEq(50) );
 }
