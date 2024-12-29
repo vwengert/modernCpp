e79d91cb2c1fb734c56f6be56d526af5efca4ed3
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-12-29 16:35:39 +0100

    Compare prices from different article

--- testdrivenlib/Tests/testsupermarketpricing.cpp
+++ testdrivenlib/Tests/testsupermarketpricing.cpp
@@ -5,16 +5,16 @@ class SupermarketPricing
   public:
     double getPrice( const char* article, const int quantity = 1 ) const
     {
-      return m_price * quantity;
+      return m_price.at( article ) * quantity;
     }
 
-    void setPrice( const int price )
+    void setPrice( const char* article, const int price )
     {
-      m_price = price;
+      m_price[ article ] = price;
     }
 
   private:
-    int m_price{ 0 };
+    std::unordered_map< std::string, double > m_price;
 };
 
 using namespace testing;
@@ -26,7 +26,7 @@ class SupermarketPricingTest : public Test
 
     void SetUp() override
     {
-      pricing.setPrice( 50 );
+      pricing.setPrice( "A", 50 );
     }
 };
 
@@ -42,6 +42,14 @@ TEST_F( SupermarketPricingTest, GetPriceOfTwoItems )
 
 TEST_F( SupermarketPricingTest, SetPriceOfItemAndGetThePriveOfThreeItems )
 {
-  pricing.setPrice( 30 );
+  pricing.setPrice( "A", 30 );
   ASSERT_THAT( pricing.getPrice( "A", 3 ), DoubleEq(90) );
+}
+
+TEST_F( SupermarketPricingTest, CompareTwoPricesToBeNotEqual )
+{
+  pricing.setPrice( "B", 50 );
+  EXPECT_THAT( pricing.getPrice("A"), DoubleEq( pricing.getPrice( "B" )) );
+  pricing.setPrice( "B", 30 );
+  ASSERT_THAT( pricing.getPrice( "A" ), Ne(pricing.getPrice( "B" )) );
 }
